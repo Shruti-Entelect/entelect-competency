7abba3092c5ba9754cf9d1b84b22f256cb7b6b23
Shruti Mondkar <shruti.mondkar@entelect.co.za>
2026-03-25 19:26:59 +0530

    Initial project commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import { Text, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Hello Shruti 🚀</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
